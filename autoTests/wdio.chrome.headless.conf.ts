dee36612f0cbfdea3ce14f782d58bf78c51848c9
Kravch773 <87003976+Kravch773@users.noreply.github.com>
2022-06-06 14:05:24 +0300

    assignment options4

--- autoTests/wdio.chrome.headless.conf.ts
+++ autoTests/wdio.chrome.headless.conf.ts
@@ -29,7 +29,7 @@ export const config: WebdriverIO.Config = {
             './test/specs/assignments/need_analysis/*.spec.ts',
         ],
         assigneeOptions: [
-            './test/specs/assignments/options/*.spec.ts'
+            './test/specs/assignments/options/5*.spec.ts'
         ],
         assignments_assignee: [
             './test/specs/assignments/assignee/*.spec.ts'
